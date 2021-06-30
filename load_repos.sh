7f4ff0f9922f769f98bc5c34df202c86a5e58a3d
Max Gotts <mgotts@princeton.edu>
2021-06-30 15:35:04 -0700

    load_repos done

---
+++ load_repos.sh
@@ -0,0 +1,9 @@
+mv ../RubensteinMpalaProject ../Mpala\ Project
+git clone git@github.com:maxgotts/RubensteinMpalaMaps.git
+mv RubensteinMpalaMaps Maps
+git clone git@github.com:maxgotts/RubensteinMpalaVegetation.git
+mv RubensteinMpalaVegetation Vegetation
+git clone git@github.com:maxgotts/RubensteinMpalaAnalysis.git
+mv RubensteinMpalaAnalysis Analysis
+git clone git@github.com:maxgotts/RubensteinWhitesheetProcessing.git
+mv RubensteinWhitesheetProcessing Whitesheets
